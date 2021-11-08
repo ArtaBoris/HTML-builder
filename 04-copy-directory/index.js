73d4db23a1f01bd94a04f7739f007903a74e0dcc
Artem Bychkov <mister.by4kov2016@yandex.ru>
2021-11-09 01:18:54 +0300

    init: add script for each task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,22 @@
+const fs = require('fs')
+const path = require('path')
 
+fs.promises.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }).then(function () {
+
+}).catch(function () {
+    console.log('failed to create directory')
+})
+
+async function clearDir() {
+    fs.readdir(path.join(__dirname, 'files-copy'), (err, files) => {
+        if (err)
+            console.log(err)
+        else {
+            files.forEach(file => {
+                fs.unlink(path.join(__dirname, 'files-copy', file), err => {
+                    if (err) throw err
+                })
+            })
+        }
+    })
+}
